b4f8ef9663bd22c09185b629ff0e79ae40556661
Varun <aitvarunbhatia@gmail.com>
2024-06-14 10:29:10 +0530

    updated code

--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,6 @@
 process.loadEnvFile();
 import { Sequelize } from "sequelize";
 
-// import mysql from "mysql2";
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// export default pool.promise();
-
 const userName = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_NAME;
