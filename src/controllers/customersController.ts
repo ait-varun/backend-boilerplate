05bb15fcaf99efd24fc7c7058c449384e4aa14f6
Varun <aitvarunbhatia@gmail.com>
2024-07-16 18:15:24 +0530

    updated code with classes

--- src/controllers/customersController.ts
+++ src/controllers/customersController.ts
@@ -2,33 +2,33 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../utils/utils";
 import customerService from "../services/customerService";
 
-const customersController = {
-  getAllCustomers: asyncHandler(async (req: Request, res: Response) => {
+class CustomersController {
+  getAllCustomers = asyncHandler(async (req: Request, res: Response) => {
     const customers = await customerService.getAllCustomers();
     res.status(200).json({ totalCustomers: customers.length, customers });
-  }),
+  });
 
-  getCustomerById: asyncHandler(async (req: Request, res: Response) => {
+  getCustomerById = asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
     const customer = await customerService.getCustomerById(Number(id));
     res.status(200).json({ customer });
-  }),
+  });
 
-  addCustomer: asyncHandler(async (req: Request, res: Response) => {
+  addCustomer = asyncHandler(async (req: Request, res: Response) => {
     const { email, name } = req.body;
     const newCustomer = await customerService.addCustomer(email, name);
     res
       .status(201)
       .json({ message: "Customer created successfully", newCustomer });
-  }),
+  });
 
-  deleteCustomer: asyncHandler(async (req: Request, res: Response) => {
+  deleteCustomer = asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
     await customerService.deleteCustomer(Number(id));
     res
       .status(200)
       .json({ message: `Customer with id ${id} deleted successfully` });
-  }),
-};
+  });
+}
 
-export default customersController;
+export default new CustomersController();
