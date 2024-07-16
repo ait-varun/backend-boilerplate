94ccfb70a099accdb139ec8e36b67c922aaad54c
Varun <aitvarunbhatia@gmail.com>
2024-07-16 17:47:14 +0530

    updated authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -10,87 +10,72 @@ import { AuthenticatedRequest } from "../interfaces/auth";
 
 const authController = {
   signup: asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const { email, password, name } = req.body;
+    const { email, password, name } = req.body;
 
-      // console.log("Received signup request for:", email);
+    // console.log("Received signup request for:", email);
 
-      if (!email || !password || !name) {
-        throw new HttpException(400, "Email, password, and name are required");
-      }
-
-      // console.log("Checking for existing user");
-      const existingUser = await Users.findByEmail(email);
-      if (existingUser) {
-        throw new HttpException(409, "Email already exists");
-      }
-
-      console.log("Hashing password");
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // console.log("Creating new user");
-      const newUser = await Users.create(email, hashedPassword, name);
-
-      // console.log("User created successfully");
-      res
-        .status(201)
-        .json({ message: "User created successfully", userId: newUser.id });
-    } catch (error) {
-      // console.error("Error in signup function:", error);
-      throw error;
+    if (!email || !password || !name) {
+      throw new HttpException(400, "Email, password, and name are required");
     }
+
+    // console.log("Checking for existing user");
+    const existingUser = await Users.findByEmail(email);
+    if (existingUser) {
+      throw new HttpException(409, "Email already exists");
+    }
+
+    console.log("Hashing password");
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // console.log("Creating new user");
+    const newUser = await Users.create(email, hashedPassword, name);
+
+    // console.log("User created successfully");
+    res
+      .status(201)
+      .json({ message: "User created successfully", userId: newUser.id });
   }),
 
   login: asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      if (!email || !password) {
-        throw new HttpException(400, "Email and password are required");
-      }
-
-      const user = await Users.findByEmail(email);
-      if (!user) {
-        throw new HttpException(401, "Invalid Email");
-      }
-
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        throw new HttpException(401, "Invalid Password");
-      }
-
-      const token = jwt.sign(
-        { userId: user.id, email: user.email },
-        process.env.JWT_SECRET!
-      );
-
-      res.cookie("token", token, {
-        domain: process.env.DOMAIN,
-        sameSite: "strict",
-        // maxAge: 60 * 1000,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-      });
-
-      res.status(200).json({ message: "Login successful!", token });
-    } catch (error) {
-      console.error("Error in login function:", error);
-      throw error;
+    if (!email || !password) {
+      throw new HttpException(400, "Email and password are required");
     }
+
+    const user = await Users.findByEmail(email);
+    if (!user) {
+      throw new HttpException(401, "Invalid Email");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new HttpException(401, "Invalid Password");
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET!
+    );
+
+    res.cookie("token", token, {
+      domain: process.env.DOMAIN,
+      sameSite: "strict",
+      // maxAge: 60 * 1000,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    res.status(200).json({ message: "Login successful!", token });
   }),
 
   getCurrentUser: asyncHandler(
     async (req: AuthenticatedRequest, res: Response) => {
-      try {
-        const user = req.user;
-        if (!user) {
-          throw new HttpException(401, "Not authenticated");
-        }
-
-        res.status(200).json({ user });
-      } catch (error) {
-        console.error("Error in getCurrentUser function:", error);
-        throw error;
+      const user = req.user;
+      if (!user) {
+        throw new HttpException(401, "Not authenticated");
       }
+
+      res.status(200).json({ user });
     }
   ),
 };
